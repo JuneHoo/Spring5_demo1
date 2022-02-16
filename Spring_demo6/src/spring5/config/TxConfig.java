f183f2e2c4b888b8a1738a19cc07d263b60dfc12
Administrator <3331562927@qq.com>
2022-02-15 16:28:42 -0800

    There

---
+++ Spring_demo6/src/spring5/config/TxConfig.java
@@ -0,0 +1,14 @@
+package spring5.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan
+public class TxConfig {
+
+
+}
